cf2382287e654b0a06b8fcd36bb8caf6c0556ef0
Doaa <doaaziadat@gmail.com>
2021-02-26 03:02:54 +0200

    add spike trigger

--- Assets/Scripts/Obstacles.cs
+++ Assets/Scripts/Obstacles.cs
@@ -8,10 +8,15 @@ public class Obstacles : MonoBehaviour
     public float min;
     public float  direction = 1;
     public float speed = 2.0f;
+    public float offest = 1;
+    public float current;
     // Start is called before the first frame update
     void Start()
     {
-
+        if(direction==1)
+        current = transform.position.x + offest;
+       else
+            current = transform.position.x - offest;
     }
 
 
@@ -25,13 +30,28 @@ public class Obstacles : MonoBehaviour
 
     public void checkDirection()
     {
-        if (transform.position.x >= max)
+      /*  if (transform.position.x >= max)
         {
             direction = -1;
         }
         else if (transform.position.x <= min)
         {
             direction = 1;
+        }
+      */
+      if(transform.position.x >=current && direction==1)
+        {
+            direction = -1;
+            current = transform.position.x - offest;
+
+
+        }
+        else if (transform.position.x < current && direction==-1)
+        {
+            direction = 1;
+            current = transform.position.x + offest;
+
+
         }
     }
 }
